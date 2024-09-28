06459b6121bc443f7f591621d1488344541c0a2d
quanha <quanha@dft.vn>
2024-09-28 19:09:14 +0700

    Update package.json: Add flag to build script and update environment file

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,16 @@
+const baseUrl = 'http://localhost:4200';
+
+export const environment = {
+  production: true,
+  application: {
+    baseUrl,
+    name: 'angular_template',
+    logoUrl: '',
+  },
+  apis: {
+    default: {
+      url: '',
+      rootNamespace: '',
+    }
+  }
+};
