ebd8a9a012935462c9591bfa56a8f7908646181a
quanha2 <quanha2@viettel.com.vn>
2025-01-04 09:45:11 +0700

    refactor-code: Refactor code and add common components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
-import { ChangeDetectionStrategy, Component, HostListener, OnInit, signal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  HostListener,
+  OnInit,
+  signal,
+} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LeftSidebarComponent } from './left-sidebar/left-sidebar.component';
+import { LeftSidebarComponent } from './components/left-sidebar/left-sidebar.component';
+import { MainLayoutComponent } from './components/main-layout/main-layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LeftSidebarComponent],
+  imports: [RouterOutlet, LeftSidebarComponent, MainLayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent implements OnInit {
   isLeftSidebarCollapsed = signal<boolean>(false);
